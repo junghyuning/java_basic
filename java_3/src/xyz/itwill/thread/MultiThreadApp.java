b631e31a8c608292d969bb8d9c9db2f0d0b64431
ITWILL <ITWILL@DESKTOP-91OIK0P>
2023-04-17 16:35:03 +0900

    MultiThread_implements_Runnable

--- java_3/src/xyz/itwill/thread/MultiThreadApp.java
+++ java_3/src/xyz/itwill/thread/MultiThreadApp.java
@@ -9,29 +9,37 @@ public class MultiThreadApp {
 //	2. 자식클래스에서 run()메서드 오버라이드 선언
 //	3. Thread클래스를 상속받은 자식클래스로 객체 생성 - Thread(부모클래스) 객체 생성
 //	4. Thread 객체로 start() 메서드 호출 - Thread 객체 이용하여 새로운 스레드 생성. -> 생성된 스레드는 자동으로 Thread 객체의 run() 메서드 호출 및 실행
-	public static void main(String[] args) {  // main스레드 (Thread1)
+	public static void main(String[] args) throws InterruptedException {  // main스레드 (Thread1)
 		
 //		MultiThreadOne one = new MultiThreadOne(); // 스레드 객체 생성 (스레드생성x)
 //		
 //		one.start();
 //		=> 아래와같이 나타내는것이 변수를 사용하지 않아 효율적.
 //		=>if one.start()를 두번 작성하면 어떨까? => one은 참조변수이며, 스레드객체 자체는 1번만 생성하였으므로 error발생함.
-		new MultiThreadOne().start();//Thread2
-		new MultiThreadOne().start();//Thread3
-		
-		
-		//if 자식클래스가 아닌 Thread 클래스의 객체 thread 이용하여 thread.start() 수행한다면?
-		// 컴파일에러x but run() 메서드가 비어있는 추상메서드이므로 실행할 내용이 없음.
-		// 따라서 run()메서드를 오버라이드한 자손 메서드를 통해 start() 호출해야함.
+//		new MultiThreadOne().start();//Thread2
+//		new MultiThreadOne().start();//Thread3
+//		
+//		
+//		//if 자식클래스가 아닌 Thread 클래스의 객체 thread 이용하여 thread.start() 수행한다면?
+//		// 컴파일에러x but run() 메서드가 비어있는 추상메서드이므로 실행할 내용이 없음.
+//		// 따라서 run()메서드를 오버라이드한 자손 메서드를 통해 start() 호출해야함.
 		for(char i='A';i<='Z';i++) {
 			System.out.print(i);
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			Thread.sleep(500);
 		}
+		
+//		CASE 1 : 
+		Runnable runnable = new MultiThreadTwo();
+		Thread thread = new Thread(runnable);
+		thread.start();
+		
+//		OR CASE 2 :
+		Thread thread2 = new Thread(new MultiThreadTwo());
+		thread2.start();
+		
+//		OR CASE 3:
+		new Thread(new MultiThreadTwo()).start();
+		
 		
 	}
 }
