26a5465dc0a6f6e72df2a8701bee701bb3fd7494
ITWILL <ITWILL@DESKTOP-91OIK0P>
2023-04-17 14:59:32 +0900

    Single Thread

--- java_3/src/xyz/itwill/thread/SingleThread.java
+++ java_3/src/xyz/itwill/thread/SingleThread.java
@@ -5,7 +5,7 @@ public class SingleThread {
 
 	public void display() {
 		System.out.println("SingleThread 클래스의 display() 메서드 시작");
-		System.out.println("["+Thread.currentThread().getName()+"] 스레드에 의해 main() 메서드의 명령 실행.");
+		System.out.println("["+Thread.currentThread().getName()+"] 스레드에 의해 SingleThread 클래스의 dispaly() 메소드 명령 실행.");
 		System.out.println("SingleThread 클래스의 display() 메서드 종료");
 	}
 
