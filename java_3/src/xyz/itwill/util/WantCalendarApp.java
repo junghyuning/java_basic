891dd850cb325fe895dbb3e2aa03835e8ab61eba
ITWILL <ITWILL@DESKTOP-91OIK0P>
2023-04-20 09:38:38 +0900

    0419

--- java_3/src/xyz/itwill/util/WantCalendarApp.java
+++ java_3/src/xyz/itwill/util/WantCalendarApp.java
@@ -11,11 +11,12 @@ public class WantCalendarApp {
 		System.out.println("출력할 연도와 월을 입력하세요.");
 		int year = scanner.nextInt();
 		int month = scanner.nextInt();
-		
 		calendar.set(year, month-1 ,1);
 		int week = calendar.get(Calendar.DAY_OF_WEEK);
-		System.out.println(week);
-		System.out.println("	       "+ calendar.get(Calendar.YEAR)+"년 "+calendar.get(Calendar.MONTH)+"월");
+		scanner.close();
+
+		System.out.println("	       "+ calendar.get(Calendar.YEAR)+"년 "+(calendar.get(Calendar.MONTH)+1)+"월");
+
 		System.out.println("==============================================");
 		System.out.println("    일    월    화    수    목    금    토   ");
 		System.out.println("==============================================");
@@ -24,7 +25,13 @@ public class WantCalendarApp {
 			System.out.print("      ");
 		}
 		
-		for()
+		for(int i = 1; i<=calendar.getActualMaximum(Calendar.DAY_OF_MONTH) ;i++) {
+			if(i<=9) {
+				System.out.print("     "+i);
+			} else System.out.print("    "+i);
+			if(week%7 == 0) System.out.println();
+			week++;
+		}
 		
 	}
 }
